b56b341e0429f6f654fe56e501cf638d4063ea7f
lewis_ljh <lewishoggarth@icloud.com>
2023-03-21 12:49:50 +0000

    Adding comments

--- src/fetch/fetchWeather.jsx
+++ src/fetch/fetchWeather.jsx
@@ -49,21 +49,29 @@ const FetchWeather = ({city}, {day = new Date().toLocaleString('en-GB', {weekday
                     <p style={{color:"orange",fontSize: "45px", padding:"0", margin:"0"}}>
                         {temp}
                         <span>&#176;</span>
+                        {/*This html/jsx code presents the windspeed,percipitation and the help button */}
                         <div class="otherdata">
+                            {/*For windspeed and precipation we combined html for the image and then preact for the API/Live data
+                            These two combine well. Using both was the right decsion as its all included within the html we could 
+                            apply CSS to all.*/}
                             <p id='windspeed'><img src="src/wind.png" width="25rem"> </img> {wind} mph</p>
                             <p id='precipation'> <img src="src/raindrop.png" width="25rem"> </img> {precip} mm</p>
+                            {/*A help box was key for us as our target audience are less tech savoy we built this using html rather
+                            then through preact as again we had more option on the design due to CSS and we could tailor it more for 
+                            our target audience. */}
                             <div class="box">
                                 <a href="#popup-box">
                                     <img src='src/questionMark.png' width="10rem"></img> 
                                     What are these?
                                 </a>
                             </div>
+                            {/*This below is the actual pop up box with the description of the symbols*/}
                             <div id="popup-box" class="modal">
                                 <div class="content">
                                     <h6 id="popup Box"><img src="src/wind.png" width="25rem"> </img>This image shows you Windspeed measured in miles per hour</h6>
                                     <h6 id="popup Box"><img src="src/raindrop.png" width="25rem"> </img>This image shows you Percipitation measured in milimeters</h6>
                                     <a href="#" 
-                                    class="box-close">
+                                    class="box-close">{/*The 'x' to close the page */}
                                         x
                                     </a>
                                 </div>
